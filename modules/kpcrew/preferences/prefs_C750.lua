509fda313a5e924d6de4d401ea59821eb7462312
Kosta Prokopiu <68238857+prokopiu@users.noreply.github.com>
2024-01-08 08:06:48 +0100

    WIP new briefing and preferences approach

--- modules/kpcrew/preferences/prefs_C750.lua
+++ modules/kpcrew/preferences/prefs_C750.lua
@@ -6,6 +6,11 @@ local C750Group = kcPreferenceGroup:new("aircraft","C750 AIRCRAFT PREFERENCES")
 C750Group:add(kcPreference:new("mcp_def_spd",	  0,	kcPreference.typeInt,	"MCP Initial Speed|5")) 
 C750Group:add(kcPreference:new("mcp_def_hdg",	001,	kcPreference.typeInt,	"MCP Initial Heading|1")) 
 C750Group:add(kcPreference:new("mcp_def_alt",	0100,	kcPreference.typeInt,	"MCP Initial Altitude|100")) 
+
 C750Group:add(kcPreference:new("powerup_ext",	false,	kcPreference.typeToggle,"Power Up|With EXT PWR|Without EXT PWR")) 
+
+C750Group:add(kcPreference:new("has_apu",		true,	kcPreference.typeToggle,"Airplane APU|Has APU|Has no APU")) 
+C750Group:add(kcPreference:new("has_gpu",		true,	kcPreference.typeToggle,"Airplane GPU|Has GPU|Has no GPU")) 
+C750Group:add(kcPreference:new("powerup_apu",	false,	kcPreference.typeToggle,"Initial Power-Up|With APU|With GPU")) 
 
 activePrefSet:addGroup(C750Group)
--- modules/kpcrew/preferences/prefs_DFLT.lua
+++ modules/kpcrew/preferences/prefs_DFLT.lua
@@ -1,11 +1,18 @@
 -- Aircraft specific preferences - Default aircraft
 --
 -- @author Kosta Prokopiu
--- @copyright 2022 Kosta Prokopiu
+-- @copyright 2023 Kosta Prokopiu
 local DFLTGroup = kcPreferenceGroup:new("aircraft","DFLT AIRCRAFT PREFERENCES")
 DFLTGroup:add(kcPreference:new("mcp_def_spd",	100,	kcPreference.typeInt,	"MCP Initial Speed|5")) 
 DFLTGroup:add(kcPreference:new("mcp_def_hdg",	001,	kcPreference.typeInt,	"MCP Initial Heading|1")) 
 DFLTGroup:add(kcPreference:new("mcp_def_alt",	4900,	kcPreference.typeInt,	"MCP Initial Altitude|100")) 
-DFLTGroup:add(kcPreference:new("powerup_apu",	false,	kcPreference.typeToggle,"Initial Power-Up|APU|GPU")) 
+
+DFLTGroup:add(kcPreference:new("has_apu",		false,	kcPreference.typeToggle,"Airplane APU|Has APU|Has no APU")) 
+DFLTGroup:add(kcPreference:new("has_gpu",		false,	kcPreference.typeToggle,"Airplane GPU|Has GPU|Has no GPU")) 
+DFLTGroup:add(kcPreference:new("powerup_apu",	false,	kcPreference.typeToggle,"Initial Power-Up|With APU|With GPU/BATT")) 
+
+DFLTGroup:add(kcPreference:new("has_retgear",	true,	kcPreference.typeToggle,"Airplane Gear|Is retractable|is fixed gear")) 
+DFLTGroup:add(kcPreference:new("mcp_def_alt",	4900,	kcPreference.typeInt,	"MCP Initial Altitude|100")) 
+
 
 activePrefSet:addGroup(DFLTGroup)
