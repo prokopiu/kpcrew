70ed3ad3f629373dee1904004597ef1442179ec3
Kosta Prokopiu <prokopiu@hotmail.com>
2025-03-14 08:26:55 +0100

    Finetuning SOP templates

--- modules/kpcrew/preferences/prefs_TMPL.lua
+++ modules/kpcrew/preferences/prefs_TMPL.lua
@@ -1,12 +1,11 @@
 -- Aircraft specific preferences - TMPL
 --
 -- @author Kosta Prokopiu
--- @copyright 2024 Kosta Prokopiu
+-- @copyright 2025 Kosta Prokopiu
 
 local TMPLGroup = kcPreferenceGroup:new("aircraft","TMPL AIRCRAFT PREFERENCES")
-MD82Group:add(kcPreference:new("mcp_def_spd",	100,	kcPreference.typeInt,	"MCP Initial Speed|5")) 
-MD82Group:add(kcPreference:new("mcp_def_hdg",	001,	kcPreference.typeInt,	"MCP Initial Heading|1")) 
-MD82Group:add(kcPreference:new("mcp_def_alt",	4900,	kcPreference.typeInt,	"MCP Initial Altitude|100")) 
-MD82Group:add(kcPreference:new("powerup_apu",	false,	kcPreference.typeToggle,"Initial Power-Up|APU|GPU")) 
+TMPLGroup:add(kcPreference:new("mcp_def_spd",	100,	kcPreference.typeInt,	"MCP Initial Speed|5")) 
+TMPLGroup:add(kcPreference:new("mcp_def_hdg",	001,	kcPreference.typeInt,	"MCP Initial Heading|1")) 
+TMPLGroup:add(kcPreference:new("mcp_def_alt",	4900,	kcPreference.typeInt,	"MCP Initial Altitude|100")) 
 
 activePrefSet:addGroup(TMPLGroup)
